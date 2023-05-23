11fe0beea4d068bce97a4effa56ffad4ec100150
Riki <ninjab02@hotmail.com>
2023-05-23 17:35:57 -0400

    initial website

---
+++ rift_watcher/src/pages/About.js
@@ -0,0 +1,12 @@
+import '../styles/About.css';
+
+function About() {
+  return (
+    <div className="AboutView">
+        <h1>This is Rift Watcher.</h1>
+        <p>blah blah blah</p>
+    </div>
+  );
+}
+
+export default About;
