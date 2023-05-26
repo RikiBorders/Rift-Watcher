ebea3106bc9f397056a4f5161c82c8de3d267ec1
Riki <ninjab02@hotmail.com>
2023-05-26 16:42:20 -0400

    finally connected the fucking backend api to the frontend. Also moved the region selection component into the search component

--- client/pages/home.tsx
+++ client/pages/home.tsx
@@ -1,17 +1,11 @@
 import NavBar from "../components/NavBar";
 import SearchBar from "../components/SearchBar";
-import RegionDropdown from "../components/RegionSelectDropdown"
 
 export default function Home() {
   return (
     <div>
       <NavBar/>
       <h1>Take Your League Journey to the Next Level.</h1>
-      <div>
-        <h3>Region</h3>
-        <RegionDropdown></RegionDropdown>
-        <h3>Summoner Name</h3>
-        <SearchBar/>
-      </div>
+      <SearchBar/>
     </div>
 );}
