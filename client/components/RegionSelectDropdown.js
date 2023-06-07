e5d65396d68006737e032dc14a8a67d447466cf9
Riki <ninjab02@hotmail.com>
2023-06-07 14:00:08 -0400

    Search bar styling v1

--- client/components/RegionSelectDropdown.js
+++ client/components/RegionSelectDropdown.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styles from "./RegionSelectDropdown.module.css"
 
 class RegionDropdown extends Component {
   constructor(props) {
@@ -18,14 +19,20 @@ class RegionDropdown extends Component {
     const { selectedOption } = this.state;
     const options = [
       { value: 'NA', label: 'North America' },
+      { value: 'KR', label: 'Korea' },
       { value: 'EUW', label: 'EU West' },
-      { value: 'EUE', label: 'EU Nordic & East' },
+      { value: 'EUN', label: 'EU Nordic & East' },
+      { value: 'LA1', label: 'Latin America North' },
+      { value: 'LA2', label: 'Latin America South' },
       { value: 'JP', label: 'Japan' },
-      { value: 'KR', label: 'Korea' }
+      { value: 'OC', label: 'Oceania' },
+      { value: 'BR', label: 'Brazil' },
+      { value: 'RU', label: 'Russia' },
+      { value: 'TR', label: 'Turkey' },
     ];
 
     return (
-      <select value={selectedOption} onChange={this.handleOptionChange}>
+      <select className={styles.select} value={selectedOption} onChange={this.handleOptionChange}>
         {options && options.map((option) => (
           <option key={option.value} value={option.value}>{option.label}</option>
         ))}
--- client/components/SearchBar.js
+++ client/components/SearchBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import RegionDropdown from "../components/RegionSelectDropdown"
+import { motion } from "framer-motion";
 import styles from "./SearchBar.module.css";
 
 class SearchBar extends Component {
@@ -7,7 +8,8 @@ class SearchBar extends Component {
     super(props);
     this.state = {
       searchTerm: '',
-      selectedRegion: 'NA'
+      selectedRegion: 'NA',
+      placeholder: 'Summoner, Champion, etc'
     };
   }
 
@@ -24,10 +26,7 @@ class SearchBar extends Component {
     // Perform search using the searchTerm
     // You can call a function or pass the search term to a parent component via props
     // For simplicity, let's just log the search term to the console
-    console.log(selectedRegion);
-    console.log('Search term:', searchTerm);
     const data = {username: searchTerm, region: selectedRegion}
-    console.log(data)
 
     const response = fetch("http://127.0.0.1:5000/get_summoner", { // update the url when pushed to prod
       method: "POST",
@@ -40,7 +39,7 @@ class SearchBar extends Component {
 
 
     if (response.ok){
-      console.log("it worked")
+      console.log("successfully sent [debug message, remove this before launch]")
     }
   }
 
@@ -51,7 +50,7 @@ class SearchBar extends Component {
 
   render() {
     const { searchTerm } = this.state;
-    const { placeholder } = this.props;
+    const { placeholder } = this.state;
 
     return (
       <div className={styles.search_bar_container}>
@@ -62,16 +61,19 @@ class SearchBar extends Component {
             <RegionDropdown getRegion={this.HandleGetRegion}/>
           </div>
 
+
           <div className={styles.input_div}>
             <h3 className={styles.search_header}>Summoner Name</h3>
-            <form onSubmit={this.handleFormSubmit}>
+            <form className={styles.search_form} onSubmit={this.handleFormSubmit}>
               <input
+                className={styles.search_input}
                 type="text"
                 placeholder={placeholder}
                 value={searchTerm}
                 onChange={this.handleInputChange}
+                size="40"
               />
-              <button type="submit">Search</button>
+              <motion.button type="submit" className={styles.search_button}/>
             </form>
           </div>
         </div>
