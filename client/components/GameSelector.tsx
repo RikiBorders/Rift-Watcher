591191af5872f06e980447d6349e681e785b2406
Riki <ninjab02@hotmail.com>
2023-06-19 12:05:25 -0400

    Added style toggle to game selector

--- client/components/GameSelector.tsx
+++ client/components/GameSelector.tsx
@@ -1,11 +1,44 @@
 import React, { use, useState } from 'react';
 import styles from "./GameSelector.module.css";
+
 export default function GameSelector({currentGame, selectGame}: {currentGame: string, selectGame: (game: string) => void}) {
     const [current, setCurrent] = useState(currentGame)
+
+    const HandleSelectGame = (gamemode: string) => {
+        if (gamemode == "LOL"){
+            selectGame("LOL")
+            setCurrent("LOL")
+        } 
+        else if (gamemode == "TFT") {
+            selectGame("TFT")
+            setCurrent("TFT")
+        }
+    } 
+
+
     return (
         <div>
-            <button className={styles.game_button_selected} onClick={() => {selectGame("LOL")}}>League of Legends</button>
-            <button className={styles.game_button} onClick={() => {selectGame("TFT")}}>Teamfight Tactics</button>
+            <button 
+                className={
+                    current == "LOL" ? 
+                    styles.game_button_selected : 
+                    styles.game_button
+                } 
+                onClick={() => {HandleSelectGame("LOL")}}
+            >
+                League of Legends
+            </button>
+
+            <button
+                className={
+                    current == "TFT" ? 
+                    styles.game_button_selected : 
+                    styles.game_button
+                } 
+                onClick={() => {HandleSelectGame("TFT")}}
+            >
+                Teamfight Tactics
+            </button>
         </div>
     )
 }
