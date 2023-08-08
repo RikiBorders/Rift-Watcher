c91b62c541fe1af5a693dc1c897e1e7a06ceb635
Riki <ninjab02@hotmail.com>
2023-08-08 15:25:52 -0400

    added almost all of the remaining tooltip icons. post-launch fix this long component and simplify it to use a render function

--- client/components/build_calculator/ItemComponent.tsx
+++ client/components/build_calculator/ItemComponent.tsx
@@ -15,10 +15,15 @@ export default function ItemComponent(props: any) {
         const has_armor = Boolean(props.item.stats.armor)
         const has_mr = Boolean(props.item.stats.magic_resist)
         const has_hp_regen = Boolean(props.item.stats.health_regen)
+        const has_mana = Boolean(props.item.stats.mana)
         const has_mana_regen = Boolean(props.item.stats.mana_regen)
         const has_armor_pen = Boolean(props.item.stats.armor_pen)
+        const has_lethality = Boolean(props.item.stats.lethality)
         const has_magic_pen = Boolean(props.item.stats.magic_pen)
         const has_movement = Boolean(props.item.stats.movement_speed)
+        const has_lifesteal = Boolean(props.item.stats.lifesteal)
+        const has_ability_haste = Boolean(props.item.stats.ability_haste)
+        const has_critical_chance = Boolean(props.item.stats.critical_chance)
 
         if (showTooltip){
             return (
@@ -49,15 +54,47 @@ export default function ItemComponent(props: any) {
                                 </div> 
                             </div>: <></>
                         }
+                        {has_critical_chance ?
+                            <div>
+                                <div className={styles.stat_row}>
+                                    <img 
+                                        src='/stat_icons/crit_chance.png' 
+                                        className={styles.tooltip_stat_icon}
+                                    />
+                                    <p className={styles.tooltip_stat_text}>{Math.round(props.item.stats.critical_chance * 100)}%</p>
+                                    <p className={styles.tooltip_stat_text_label}>Critical Chance</p>
+                                </div> 
+                            </div>: <></>
+                        }
+                        {has_lifesteal ?
+                            <div>
+                                <div className={styles.stat_row}>
+                                    <img 
+                                        src='/stat_icons/life_steal.png' 
+                                        className={styles.tooltip_stat_icon}
+                                    />
+                                    <p className={styles.tooltip_stat_text}>{Math.round(props.item.stats.lifesteal * 100)}%</p>
+                                    <p className={styles.tooltip_stat_text_label}>Lifesteal</p>
+                                </div> 
+                            </div>: <></>
+                        }
                         {has_armor_pen ?
                             <div>
                                 <div className={styles.stat_row}>
                                     <img 
-                                        src='https://raw.communitydragon.org/latest/game/assets/perks/statmods/statmodsattackdamageicon.png' 
+                                        src='/stat_icons/armor_pen.png' 
                                         className={styles.tooltip_stat_icon}
                                     />
                                     <p className={styles.tooltip_stat_text}>{Math.round(props.item.stats.armor_pen * 100)}% </p>
                                     <p className={styles.tooltip_stat_text_label}>Armor Penetration</p>
+                                </div> 
+                            </div>: <></>
+                        }
+                        {has_lethality ?
+                            <div>
+                                <div className={styles.stat_row}>
+                                    <p className={styles.lethality_tooltip_stat_text}>+{props.item.stats.lethality} </p>
+                                    <p className={styles.tooltip_stat_text_label}>Lethality</p>
                                 </div> 
                             </div>: <></>
                         }
@@ -75,22 +112,43 @@ export default function ItemComponent(props: any) {
                             <div>
                                 <div className={styles.stat_row}>
                                     <img 
-                                        src='https://raw.communitydragon.org/latest/game/assets/perks/statmods/statmodsattackdamageicon.png' 
+                                        src='/stat_icons/magic_pen.png' 
                                         className={styles.tooltip_stat_icon}
                                     />
-                                    <p className={styles.tooltip_stat_text}>{Math.round(props.item.stats.magic_pen * 100)}% </p>
-                                    <p className={styles.tooltip_stat_text_label}>Armor Penetration</p>
+                                    <p className={styles.tooltip_stat_text}>{props.item.stats.magic_pen}% </p>
+                                    <p className={styles.tooltip_stat_text_label}>Magic Penetration</p>
                                 </div> 
                             </div>: <></>
+                        }
+
+                        {has_mana ?
+                            <div className={styles.stat_row}>
+                                <img 
+                                    src='/stat_icons/mana.png' 
+                                    className={styles.tooltip_stat_icon}
+                                />
+                                <p className={styles.tooltip_stat_text}>{props.item.stats.mana} </p>
+                                <p className={styles.tooltip_stat_text_label}>Mana</p>
+                            </div> : <></>
                         }
                         {has_mana_regen ?
                             <div className={styles.stat_row}>
                                 <img 
-                                    src='https://raw.communitydragon.org/latest/game/assets/perks/statmods/statmodshealthscalingicon.png' 
+                                    src='/stat_icons/mana_regen.png' 
                                     className={styles.tooltip_stat_icon}
                                 />
                                 <p className={styles.tooltip_stat_text}>{props.item.stats.mana_regen} </p>
                                 <p className={styles.tooltip_stat_text_label}>Mana Regen</p>
+                            </div> : <></>
+                        }
+                        {has_ability_haste ?
+                            <div className={styles.stat_row}>
+                                <img 
+                                    src='https://raw.communitydragon.org/latest/game/assets/perks/statmods/statmodscdrscalingicon.png' 
+                                    className={styles.tooltip_stat_icon}
+                                />
+                                <p className={styles.tooltip_stat_text}>{props.item.stats.ability_haste} </p>
+                                <p className={styles.tooltip_stat_text_label}>Ability Haste</p>
                             </div> : <></>
                         }
                         {has_as ?
@@ -116,7 +174,7 @@ export default function ItemComponent(props: any) {
                         {has_hp_regen ?
                             <div className={styles.stat_row}>
                                 <img 
-                                    src='https://raw.communitydragon.org/latest/game/assets/perks/statmods/statmodshealthscalingicon.png' 
+                                    src='/stat_icons/hp_regen.png' 
                                     className={styles.tooltip_stat_icon}
                                 />
                                 <p className={styles.tooltip_stat_text}>{props.item.stats.health_regen} </p>
@@ -142,7 +200,17 @@ export default function ItemComponent(props: any) {
                                 <p className={styles.tooltip_stat_text}>{props.item.stats.magic_resist} </p>
                                 <p className={styles.tooltip_stat_text_label}>Magic Resist</p>
                             </div> : <></>
-                        }                        
+                        }    
+                        {has_movement ?
+                            <div className={styles.stat_row}>
+                                <img 
+                                    src='https://raw.communitydragon.org/latest/game/assets/perks/statmods/statmodsmovementspeedicon.png' 
+                                    className={styles.tooltip_stat_icon}
+                                />
+                                <p className={styles.tooltip_stat_text}>{props.item.stats.movement_speed} </p>
+                                <p className={styles.tooltip_stat_text_label}>Movement Speed</p>
+                            </div> : <></>
+                        }                      
                     </div>
                     
                 </div>
